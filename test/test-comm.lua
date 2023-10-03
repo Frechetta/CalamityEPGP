59bded33a17e73ce279700a1fdecfd145da229ba
Eric Frechette <frechetta93@gmail.com>
2023-10-03 02:11:08 -0500

    github actions lua-wow

--- test/test-comm.lua
+++ test/test-comm.lua
@@ -12,92 +12,92 @@ describe('encodeEvent', function()
         Util:loadModule('comm', ns)
     end)
 
---     test('test', function()
---         local eventAndHash = {
---             {
---                 1690330283,
---                 'p1',
---                 {
---                     'p1',
---                     'p2',
---                     'p3',
---                 },
---                 'ep',
---                 20,
---                 '1:wee'
---             },
---             2074722407
---         }
+    test('test', function()
+        local eventAndHash = {
+            {
+                1690330283,
+                'p1',
+                {
+                    'p1',
+                    'p2',
+                    'p3',
+                },
+                'ep',
+                20,
+                '1:wee'
+            },
+            2074722407
+        }
 
---         local expectedEncodedEventAndHash = {
---             {
---                 'BkwGSr',
---                 'p1',
---                 {
---                     'p1',
---                     'p2',
---                     'p3',
---                 },
---                 'ep',
---                 20,
---                 '1:wee'
---             },
---             'B7qcBn'
---         }
+        local expectedEncodedEventAndHash = {
+            {
+                'BkwGSr',
+                'p1',
+                {
+                    'p1',
+                    'p2',
+                    'p3',
+                },
+                'ep',
+                20,
+                '1:wee'
+            },
+            'B7qcBn'
+        }
 
---         local actualEncodedEventAndHash = ns.Comm.encodeEvent(eventAndHash)
+        local actualEncodedEventAndHash = ns.Comm.encodeEvent(eventAndHash)
 
---         assert.same(expectedEncodedEventAndHash, actualEncodedEventAndHash)
---     end)
--- end)
+        assert.same(expectedEncodedEventAndHash, actualEncodedEventAndHash)
+    end)
+end)
 
 
--- describe('decodeEvent', function()
---     local ns
+describe('decodeEvent', function()
+    local ns
 
---     before_each(function()
---         ns = {}
+    before_each(function()
+        ns = {}
 
---         Util:loadModule('datatypes', ns)
---         Util:loadModule('lib', ns)
---         Util:loadModule('comm', ns)
---     end)
+        Util:loadModule('datatypes', ns)
+        Util:loadModule('lib', ns)
+        Util:loadModule('comm', ns)
+    end)
 
---     test('test', function()
---         local encodedEventAndHash = {
---             {
---                 'BkwGSr',
---                 'p1',
---                 {
---                     'p1',
---                     'p2',
---                     'p3',
---                 },
---                 'ep',
---                 20,
---                 '1:wee'
---             },
---             'B7qcBn'
---         }
+    test('test', function()
+        local encodedEventAndHash = {
+            {
+                'BkwGSr',
+                'p1',
+                {
+                    'p1',
+                    'p2',
+                    'p3',
+                },
+                'ep',
+                20,
+                '1:wee'
+            },
+            'B7qcBn'
+        }
 
---         local expectedEventAndHash = {
---             {
---                 1690330283,
---                 'p1',
---                 {
---                     'p1',
---                     'p2',
---                     'p3',
---                 },
---                 'ep',
---                 20,
---                 '1:wee'
---             },
---             2074722407
---         }
+        local expectedEventAndHash = {
+            {
+                1690330283,
+                'p1',
+                {
+                    'p1',
+                    'p2',
+                    'p3',
+                },
+                'ep',
+                20,
+                '1:wee'
+            },
+            2074722407
+        }
 
---         local actualEventAndHash = ns.Comm.decodeEvent(encodedEventAndHash)
+        local actualEventAndHash = ns.Comm.decodeEvent(encodedEventAndHash)
 
---         assert.same(expectedEventAndHash, actualEventAndHash)
---     end)
+        assert.same(expectedEventAndHash, actualEventAndHash)
+    end)
 end)
