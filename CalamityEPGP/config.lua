bcd2ec4a28d0319a4375a7cc16420040da8fcb3f
Eric Frechette <frechetta93@gmail.com>
2025-10-15 23:48:48 -0500

    fix: replace GRM_Alts[ns.guild] with GRM.GetGuildAlts()

--- CalamityEPGP/config.lua
+++ CalamityEPGP/config.lua
@@ -341,12 +341,12 @@ function Config:createAltManagementMenu()
     panel.tableFrame:SetPoint('BOTTOMRIGHT', panel.synchroniseEpCheck, 'TOPRIGHT', 0, 15)
 
     panel.importAltMappingButton:SetScript('OnClick', function()
-        if GRM_Alts == nil then
+        if GRM == nil then
             ns.print('GRM data not accessible')
             return
         end
 
-        for _, altData in pairs(GRM_Alts[ns.guild]) do
+        for _, altData in pairs(GRM.GetGuildAlts()) do
             if #altData.main > 0 then
                 local main = ns.addon.getCharName(altData.main)
 
