e665e0805a492a4af62039bf8e794cefcf2b2f84
Eric Frechette <frechetta93@gmail.com>
2025-10-15 23:47:48 -0500

    fix: replace call to GetLoodMethod() to C_PartyInfo.GetLootMethod()

--- CalamityEPGP/main.lua
+++ CalamityEPGP/main.lua
@@ -1335,14 +1335,14 @@ end
 function addon:handleEnteredRaid()
     self:loadRaidRoster()
 
-    if ns.cfg and ns.cfg.lmMode and GetLootMethod() == 'master' and IsMasterLooter() and not self.useForRaidPrompted then
+    if ns.cfg and ns.cfg.lmMode and C_PartyInfo.GetLootMethod() == 'master' and IsMasterLooter() and not self.useForRaidPrompted then
         self:showUseForRaidWindow()
     end
 end
 
 
 function addon:handlePartyLootMethodChanged()
-    if ns.cfg and ns.cfg.lmMode and GetLootMethod() == 'master' and IsMasterLooter() then
+    if ns.cfg and ns.cfg.lmMode and C_PartyInfo.GetLootMethod() == 'master' and IsMasterLooter() then
         if not self.useForRaid then
             self:showUseForRaidWindow()
         end
